6887e58659e18607e09a6342789486a16c0e46bd
Son Ma <t.son.ma@outlook.com>
2024-11-09 10:29:03 -0500

    fixed the formatting and added git setup instructions

---
+++ Theory/0.1.Setup_Virtual_Environment.py
@@ -0,0 +1,8 @@
+# Step 1 Create Virtual Environment in Terminal
+# python -m venv env
+
+# Step 2 Activate in terminal
+# .\env\Scripts\activate
+
+# Step 3 Deactivate in terminal
+# deactivate
---
+++ Theory/0.2.Setup_Git.py
@@ -0,0 +1,19 @@
+# All steps need to be done in the terminal
+
+# Create a folder
+# mkdir folder_name
+
+# How to change directory
+# cd folder_name
+
+# Initialize the git in the folder created
+# git init
+
+# How to add all changes in the staging file. This will ignore the data from .gitignore
+# git add .
+
+# How to commit the changes
+# git commit -m 'first working file of project. You can add any relevant description'
+
+# How to publish to GitHub
+# git remote add origin https://github.com/use_name/data_cleaning_maven_analytics.git
